3cfa1b4c7909f0a142e4707c14c7b7e24620d379
iFatma2 <fatma.m.q@outlook.sa>
2024-09-26 11:53:10 +0300

    change padding 20

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -1,25 +1,38 @@
 //
 //  ContentView.swift
-//  GitHubDemo
+//  Demoapp1
 //
-//  Created by Fatimah Alqarni on 23/03/1446 AH.
-//
+//  Created by Fatimah Alqarni on 22/03/1446 AH.
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Text("Water Tracker💦").font(.largeTitle).fontWeight(.bold)
+
+
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+            Text("Apple Helth").padding(20)
+                
+            }
+            
+            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+                Text("Cups to drink per day 0").padding()
+            }
+            
+            
+            Button("Continue") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
-        .padding()
+        .padding(.all, 15.0)
+    
     }
+
 }
 
 #Preview {
     ContentView()
 }
-Ensure the author information supplied in Xcode > Settings is correct then create the git repository using Source Control > New Git Repository…
